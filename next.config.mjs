1d69960d0efc6f9217cabeea789d57f535ea48a0
mantupatra23-pixel <apna-asli-email@gmail.com>
2026-08-28 01:09:59 +0530

    fix: set output export for static site build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
